9425f73fcb5ee082a3f0cc1fae011215e0bcf628
David R. Bild <david.bild@xaptum.com>
2017-04-24 16:38:50 -0500

    remove  reference

--- .config/zsh/zsh/config.zsh
+++ .config/zsh/zsh/config.zsh
@@ -28,8 +28,6 @@ setopt HIST_REDUCE_BLANKS
 #   like: git comm-[tab]
 setopt complete_aliases
 
-zle -N newtab
-
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
 bindkey '^[[5D' beginning-of-line
