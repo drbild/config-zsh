666502db0fbee9ed1ba15608e8e90646a47efb90
David R. Bild <david.bild@xaptum.com>
2018-04-19 18:08:57 -0500

    ccache: use scripts instead of aliases to invoke

--- .config/zsh/ccache/env.zsh
+++ .config/zsh/ccache/env.zsh
@@ -1,5 +1,5 @@
 if which ccache > /dev/null; then
   export CCACHE_DIR="${HOME}/.ccache"
-  export CC="ccache gcc"
-  export CXX="ccache g++"
+  export CC="ccache-gcc"
+  export CXX="ccache-g++"
 fi
