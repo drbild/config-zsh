c2ce0ef996014c90c6fba9e804d80d8f36105cd3
David R. Bild <david.bild@xaptum.com>
2018-06-26 20:29:56 -0500

    ccache: use clang, not gcc, on Darwin

--- .config/zsh/ccache/env.zsh
+++ .config/zsh/ccache/env.zsh
@@ -1,5 +1,10 @@
 if which ccache > /dev/null; then
   export CCACHE_DIR="${HOME}/.ccache"
-  export CC="ccache-gcc"
-  export CXX="ccache-g++"
+  if [[ `uname` == 'Darwin' ]]; then
+    export CC="ccache-clang"
+    export CXX="ccache-clang"
+  else
+    export CC="ccache-gcc"
+    export CXX="ccache-g++"
+  fi
 fi
