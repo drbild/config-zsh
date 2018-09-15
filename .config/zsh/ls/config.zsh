6cdf6676c799131abd456a1df9117ffeadcde5bb
David R. Bild <drbild@willbild.com>
2018-09-15 15:31:31 -0500

    ls: do not sort by ctime

--- .config/zsh/ls/config.zsh
+++ .config/zsh/ls/config.zsh
@@ -8,5 +8,5 @@ case `uname -s` in
     (Linux)
 	;&
     (*)
-	export LS_OPTIONS="${LS_OPTIONS} -c -h --group-directories-first"
+	export LS_OPTIONS="${LS_OPTIONS} -h --group-directories-first"
 esac
