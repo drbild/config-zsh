e69c3e0d4a19c824e69690cdf0271819423c47b7
David R. Bild <david.bild@xaptum.com>
2019-02-05 20:00:23 -0600

    ccache: use correct wrapper script for C++

--- .config/zsh/ccache/env.zsh
+++ .config/zsh/ccache/env.zsh
@@ -2,7 +2,7 @@ if which ccache > /dev/null; then
   export CCACHE_DIR="${HOME}/.ccache"
   if [[ `uname` == 'Darwin' ]]; then
     export CC="ccache-clang"
-    export CXX="ccache-clang"
+    export CXX="ccache-clang++"
   else
     export CC="ccache-gcc"
     export CXX="ccache-g++"
