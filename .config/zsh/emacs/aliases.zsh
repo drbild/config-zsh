27a48615b81dd4e51ec592f2004cff70fc3da331
David R. Bild <david.bild@resourcely.io>
2024-08-11 15:14:54 -0500

    emacs: update emacs alias to work inside of vterm

--- .config/zsh/emacs/aliases.zsh
+++ .config/zsh/emacs/aliases.zsh
@@ -1,2 +1,30 @@
 alias emcas='emacs'
-alias emacs='emacsclient -create-frame --alternate-editor=""'
+
+if [[ "$INSIDE_EMACS" = 'vterm' ]]; then
+  alias emacs='emacsclient_vterm'
+else
+  alias emacs='emacsclient -create-frame --alternate-editor=""'
+fi
+
+# Launch emacsclient inside of vterm.
+#
+# Strips any args that would try to create a new frame, ensuring that the
+# requested buffer the frame hosting the vterm instance.
+#
+# Trying to create new frame can cause the emacs server to freeze.
+function emacsclient_vterm() {
+    local args=()
+
+    for arg in "$@"; do
+      case "$arg" in
+        -c|--create-frame|-nw|-t|--tty)
+          # Drop args that would create a new frame
+          ;;
+        *)
+          args+=("$arg")
+          ;;
+      esac
+    done
+
+    emacsclient "${args[@]}"
+}
